c2d570c77101b3e7fc740323a382c248df870a3e
unknown <tiduy96@gmail.com>
2020-05-25 12:19:54 -0400

    working edition

--- plugin/background.js
+++ plugin/background.js
@@ -1,19 +1,69 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
-    });
+var listURL = new Array();
+var listTimestamp = new Array();
+var username;
 
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
-          conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals: 'sites.google.com'},
-          })
-          ],
-              actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-      });
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  // chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  var d = new Date();
+  var url;
+
+  chrome.tabs.query({ 'active': true, 'currentWindow': true }, function (tabs) {
+    url = tabs[0].url;
   });
 
+  chrome.storage.sync.get('url', function (result) {
+    if (result.url != null) {
+      listURL = result.url;
+      listURL.push(url);
+    }
+  });
+
+  chrome.storage.sync.set({ 'url': listURL }, function () { });
+
+  chrome.storage.sync.get('time', function (result) {
+    if (result.time != null) {
+      listTimestamp = result.time;
+      listTimestamp.push(d.toLocaleTimeString());
+    }
+  });
+  chrome.storage.sync.set({ 'time': listTimestamp }, function () { });
+
+});
 
 
- 
+
+
+/* Upon receiving the 'download' message from popup.js, get items from storage, format, and download the file */
+chrome.runtime.onMessage.addListener(
+  function (arg, sender) {
+    if (arg.message === 'download') {
+      chrome.storage.sync.get(null, function (items) { // null implies all items
+        // Convert object to a string.
+        var result = JSON.stringify(items);
+
+        // Save as file
+        var url = 'data:application/json;base64,' + btoa(result);
+        chrome.downloads.download({
+          url: url,
+          filename: '000tutorial.txt',
+          conflictAction: "overwrite",
+          saveAs: false,
+        });
+      });
+    }
+  }
+);
+
+
+chrome.runtime.onMessage.addListener(
+  function (arg, sender) {
+    if (arg.message === 'saveUser') {
+      chrome.storage.sync.set({ 'user': username }, function () { });
+    }
+  }
+);
+
+
+
+
+
--- plugin/popup.js
+++ plugin/popup.js
@@ -1,137 +1,44 @@
-
-
-var listURL = new Array();
-var listTimestamp = new Array();
-// var listFinal = new Array();
-
-
-chrome.storage.local.get('url', function(result){
-  if(result.url != null) {
-    listURL = result.url;
-  }
-});
-
-
-
-
-
-
-var myVar = setInterval(myTimer, 5000);
-
-function myTimer() {
-  var d = new Date();
-  var url;
-  document.getElementById("demo2").textContent = d.toLocaleTimeString();
-
-  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    url = tabs[0].url; 
-    document.getElementById('demo3').textContent = url;
-    
-  });
-
-  chrome.storage.local.get('url', function(result){
-    if(result.url != null) {
-      listURL = result.url;
-      listURL.push(url);
-    }
-  });
-  //alert(listURL);
-  chrome.storage.local.set({'url': listURL }, function(){});
-  
-
-  //document.getElementById('demo3').textContent = window.location;
-  chrome.storage.local.get('time', function(result){
-    if(result.time != null) {
-        listTimestamp = result.time;
-        listTimestamp.push(d.toLocaleTimeString());
-    }
-  });
-  chrome.storage.local.set({'time': listTimestamp }, function(){});
-  
-  
-  
-}
+var submitted = 0;
 
 
 
 document.addEventListener('DOMContentLoaded', function () {
-	var button = document.getElementById("download");
-	button.addEventListener('click', function(response) {
-		chrome.runtime.sendMessage({from: 'popup'});
-	});
-});
+  if (submitted == 0) {
+    document.getElementById('prompt').hidden = false;
+    document.getElementById('submit').hidden = false;
+    document.getElementById('username').hidden = false;
+  }
+
+  var button = document.getElementById('download');
+  button.addEventListener('click', function (response) {
+    chrome.runtime.sendMessage({ 'message': 'download' });
+  });
 
 
-
-
-  document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('demo').textContent = "Extension loaded";
-    var tryit = document.getElementById('try');
-
-    tryit.addEventListener('click', function () {
-      document.getElementById('demo').textContent = "You clicked on Try It!";
-    });
-});
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
   var button = document.getElementById('submit');
   button.addEventListener('click', function () {
-      var text = document.getElementsByName('username')[0].value;
-      users.push(text);
-      var grpnum = Math.random();
-     
-      if (grpnum < 0.5) {
-          grpnum = 1;
-          document.getElementById('discovery').hidden = false;
-          document.getElementById('shell').hidden = true;
-      }
-      else{
-        grpnum = 2;
-        document.getElementById('discovery').hidden = true;
-        document.getElementById('shell').hidden = false;
-      }
-      var mess = text + ", you are in group " + grpnum + "!";
-      document.getElementById('demo').innerHTML = mess;
-      
-      
-      
-  });
-
-  
-  var button = document.getElementById("download");
-	button.addEventListener('click', function(response) {
-
-    chrome.storage.local.get('time', function(result){
-      if(result.time != null) {
-          listTimestamp = result.time;
-      }
-    });
-
-    chrome.storage.local.get('url', function(result){
-      if(result.url != null) {
-          listURL = result.url;
-      }
-    });
-
-    if(listTimestamp.length == listURL.length) {
-      finalList = new Array();
-      var i;
-      for (i = 0; i < clickList.length; i++) {
-        var str1 = '[' + listTimestamp[i] + ',' + listURL[i] + ']';
-        finalList.push(str1);
-      }
+    submited = 1;
+    document.getElementById('prompt').hidden = true;
+    document.getElementById('submit').hidden = true;
+    document.getElementById('username').hidden = true;
+    var text = document.getElementsByName('username')[0].value;
+    document.getElementById('demo').innerHTML = text;
+    chrome.runtime.sendMessage({ 'message': 'saveUser', 'username': text });
+    var grpnum = Math.random();
+    if (grpnum < 0.5) {
+      grpnum = 1;
+      document.getElementById('discovery').hidden = false;
+      document.getElementById('shell').hidden = true;
     }
+    else {
+      grpnum = 2;
+      document.getElementById('discovery').hidden = true;
+      document.getElementById('shell').hidden = false;
+    }
+    var mess = text + ", you are in group " + grpnum + "!";
+    document.getElementById('demo').innerHTML = mess;
 
-    chrome.downloads.download({
-      url: "data:text/plain," + listURL,
-      filename: "tutorial.txt",
-      conflictAction: "overwrite", 
-      saveAs: false,
   });
-  });
-
 });
 
 
